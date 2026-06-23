946f6bad936b7859ea4d705d0a7a9ddae7ec4721
Abhishek Prajapatt <prajapattabhishek@gmail.com>
2026-06-23 23:26:41 +0530

    Rabbit MERN Learning Management System

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
 import dotenv from "dotenv";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
